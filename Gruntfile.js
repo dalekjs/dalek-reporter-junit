44f91d77eb014e6b2651d2861bf09630dcf82ce5
asciidisco <public@asciidisco.com>
2014-03-24 17:56:08 +0100

    chore(gruntfile): updated Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -102,7 +102,7 @@ module.exports = function (grunt) {
           },
         },
         src: 'report/docs/*.html',
-        dest: '.'
+        dest: './'
       }
     },
 
@@ -125,7 +125,6 @@ module.exports = function (grunt) {
   // load 3rd party tasks
   require('load-grunt-tasks')(grunt);
   grunt.loadTasks('./node_modules/dalek-build-tools/tasks');
-  grunt.loadNpmTasks('grunt-documantix');
 
   // define runner tasks
   grunt.registerTask('lint', 'jshint');
